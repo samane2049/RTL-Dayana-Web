5dabdcf7cddeda44fb88497bf7cc2caf3a6b73cc
Admin <gholmzadehs@gmail.com>
2022-04-10 12:19:40 +0430

    Repozitory initialized

---
+++ Rtl-Dayana-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rtl_Dayana_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
