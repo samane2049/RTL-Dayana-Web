4ebe1d0e18d5813308db973fd6d7cc59a0543101
Admin <gholmzadehs@gmail.com>
2022-04-12 10:30:27 +0430

    make about page

--- Rtl-Dayana-web/Controllers/HomeController.cs
+++ Rtl-Dayana-web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Rtl_Dayana_web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
